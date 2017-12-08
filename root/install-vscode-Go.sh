198b7001c144d34a76755a05a372ece3a7ad18f4
Grant Gillespie <gillegra@gmail.com>
2017-12-08 09:36:41 -0600

    update vscode go extension installation script from documentation

--- root/install-vscode-Go.sh
+++ root/install-vscode-Go.sh
@@ -3,23 +3,29 @@ dir=$(cd `dirname "$0"` && pwd)
 # publisher extension version
 $dir/install-vscode-extension.sh lukehoban Go 0.6.69
 
-# see https://github.com/ctaggart/golang-vscode/issues/8
-#echo 'export GOPATH=$HOME/go' >> ~/.bash_profile
-#echo 'export PATH=$GOPATH/bin:/usr/local/go/bin:$PATH' >> ~/.bash_profile
+#see https://github.com/ctaggart/golang-vscode/issues/8
+# echo 'export GOPATH=$HOME/go' >> ~/.bash_profile
+# echo 'export PATH=$GOPATH/bin:/usr/local/go/bin:$PATH' >> ~/.bash_profile
 export GOPATH=$HOME/go
 echo 'export PATH=~/go/bin:/usr/local/go/bin:$PATH' >> ~/.bash_profile
 . ~/.bash_profile
 
-# https://github.com/Microsoft/vscode-go#tools
+#https://github.com/Microsoft/vscode-go/wiki/Go-tools-that-the-Go-extension-depends-on
+go get -u -v github.com/ramya-rao-a/go-outline
+go get -u -v github.com/acroca/go-symbols
 go get -u -v github.com/nsf/gocode
 go get -u -v github.com/rogpeppe/godef
+go get -u -v golang.org/x/tools/cmd/godoc
+go get -u -v github.com/zmb3/gogetdoc
 go get -u -v github.com/golang/lint/golint
-go get -u -v github.com/lukehoban/go-outline
-go get -u -v sourcegraph.com/sqs/goreturns
+go get -u -v github.com/fatih/gomodifytags
+go get -u -v github.com/uudashr/gopkgs/cmd/gopkgs
 go get -u -v golang.org/x/tools/cmd/gorename
-go get -u -v github.com/tpng/gopkgs
-go get -u -v github.com/newhook/go-symbols
+go get -u -v sourcegraph.com/sqs/goreturns
+go get -u -v github.com/cweill/gotests/...
 go get -u -v golang.org/x/tools/cmd/guru
+go get -u -v github.com/josharian/impl
+go get -u -v github.com/haya14busa/goplay/cmd/goplay
 
-# https://github.com/derekparker/delve/blob/master/Documentation/installation/linux/install.md
+#https://github.com/derekparker/delve/blob/master/Documentation/installation/linux/install.md
 go get github.com/derekparker/delve/cmd/dlv
