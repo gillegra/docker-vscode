61675b032367827d20eb0ba219aff72917d315ec
Grant Gillespie <gillegra@gmail.com>
2017-12-08 09:46:02 -0600

    add docker installation script

--- root/install-apt-packages.sh
+++ root/install-apt-packages.sh
@@ -1,5 +1,20 @@
 #!/bin/sh
-apt-get update \
+#set up the docker repository and then install the docker engine
+curl -fsSL https://download.docker.com/linux/$(. /etc/os-release; echo "$ID")/gpg | sudo apt-key add - \
+: <<COMMENT \
+#should check the key fingerprint? \
+COMMENT \
+&& add-apt-repository \
+  "deb [arch=amd64] https://download.docker.com/linux/$(. /etc/os-release; echo "$ID") \
+  $(lsb_release -cs) \
+  stable" \
+\
+&& apt-get update \
+&& apt-get install docker-ce \
+\
+: <<COMMENT \
+#install vscode and its dependencies
+COMMENT \
 && apt-get install -y --no-install-recommends unzip \
   libgtk2.0-0 libxss1 libx11-xcb1 libxtst6 libasound2 \
   libnotify4 libnss3 libxkbfile1 libgconf-2-4 libsecret-1-0 \
